c31ededde9067dfb1054e0757b38d715ec16a63a
Justin Spahr-Summers <justin@jspahrsummers.com>
2019-04-19 14:42:22 +0100

    Define `values` even if not fetching

--- bankroll.py
+++ bankroll.py
@@ -97,6 +97,7 @@ dataProvider: Optional[LiveDataProvider] = None
 
 
 def printPositions(args: Namespace) -> None:
+    values: Dict[Position, Cash] = {}
     if args.live_value:
         assert dataProvider, 'Live data connection required to fetch market values'
         values = analysis.liveValuesForPositions(positions,
