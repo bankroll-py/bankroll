f7c4c2e97750fc3a1bca69c25471758e6817cea0
Justin Spahr-Summers <justin@jspahrsummers.com>
2019-05-11 20:37:51 +0100

    Bump version to 0.2.0
    Breaking changes in CLI arguments since last release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='bankroll',
-    version='0.1.2',
+    version='0.2.0',
     author='Justin Spahr-Summers',
     author_email='justin@jspahrsummers.com',
     description=
