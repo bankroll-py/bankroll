8ae32385ab50b083cd89afca5e84a1a41e8a13e8
Justin Spahr-Summers <justin@jspahrsummers.com>
2019-05-11 11:41:43 +0100

    Refactor `config` to be more OO
    Also support overriding settings with a mapping, which can be used for CLI args

---
+++ bankroll/configuration.py
@@ -0,0 +1,46 @@
+from configparser import ConfigParser
+from enum import Enum, unique
+from itertools import chain
+from typing import Dict, Generic, Iterable, Mapping, Type, TypeVar
+
+import os
+import pkg_resources
+
+
+@unique
+class Settings(str, Enum):
+    @classmethod
+    def sectionName(cls) -> str:
+        pass
+
+
+_S = TypeVar('_S', bound=Settings)
+
+
+class Configuration:
+    _defaultConfigName = 'bankroll.default.ini'
+
+    def __init__(self, extraSearchPaths: Iterable[str] = []):
+        self._config = ConfigParser(empty_lines_in_values=False)
+
+        defaultConfig = pkg_resources.resource_string('bankroll',
+                                                      self._defaultConfigName)
+        self._config.read_string(defaultConfig.decode(),
+                                 self._defaultConfigName)
+
+        self._config.read(
+            chain(extraSearchPaths,
+                  ['bankroll.ini',
+                   os.path.expanduser('~/.bankroll.ini')]))
+
+    def section(self, settings: Type[_S],
+                overrides: Mapping[_S, str] = {}) -> Dict[_S, str]:
+        elements: Iterable[_S] = list(settings)
+
+        optionalValues = {
+            key: overrides[key]
+            or self._config.get(settings.sectionName(), key.value, fallback='')
+            for key in elements
+        }
+
+        return {key: value for key, value in optionalValues.items() if value}
