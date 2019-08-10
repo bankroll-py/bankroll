e5715e39ef47328a532b0e84678ce7f1e62c89bd
Justin Spahr-Summers <justin@jspahrsummers.com>
2019-08-10 21:11:32 +0100

    Bump version to 0.3.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='bankroll',
-    version='0.3.1',
+    version='0.3.2',
     author='Justin Spahr-Summers',
     author_email='justin@jspahrsummers.com',
     description=
